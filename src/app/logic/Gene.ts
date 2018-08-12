1042924e509047f82b461c2360113dde7d19c6b6
stefian <v_ianta@yahoo.com>
2018-08-12 11:38:02 -0400

    added 3 logic classes

---
+++ src/app/logic/Gene.ts
@@ -0,0 +1,13 @@
+class Gene {
+
+    // Properties
+    name: string;
+    place: string;
+    industry: string;
+    type: string;
+    location: PlaceLocation;
+    rating: number;
+    notes: string;
+    tastingRating: TastingRating;
+
+}
