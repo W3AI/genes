b547f8371d60a73a40836156fd33b779179662f5
stefian <v_ianta@yahoo.com>
2018-08-13 19:42:22 -0400

    connected mobile actions and included manifest.json and icons

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from "../data.service";
 import { Gene } from '../logic/Gene';
 import { Router } from '@angular/router';
+import { GeolocationService } from '../geolocation.service';
 
 @Component({
   selector: 'app-list',
@@ -13,10 +14,30 @@ export class ListComponent implements OnInit {
   list : [Gene]
 
   constructor(private data: DataService,
-              private router: Router) { }
+              private router: Router,
+              private geolocation: GeolocationService) { }
 
   goDetails(gene: Gene) {
     this.router.navigate(["/gene", gene._id])
+  }
+
+  goMap(gene: Gene) {
+    const mapURL = this.geolocation.getMapLink(gene.location);
+    location.href = mapURL;
+  }
+
+  share(gene: Gene) {
+    const shareText = `I got this skill at a training class in ${gene.place} and for me it's a ${gene.rating} skill`;
+    if ('share' in navigator) {
+      navigator["share"]({
+        title: gene.name,
+        text: shareText,
+        url: window.location.href
+      }).then( () => console.log("shared")).catch( () => console.log("error sharing"));
+    } else {
+      const shareURL = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+      location.href = shareURL;
+    }
   }
 
   ngOnInit() {
